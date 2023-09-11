fa75270263473d5cc5a7385e1138b45272fb0af0
tintun123 <mr.tintunoo1997@gmail.com>
2023-09-11 13:34:19 +0630

    4 commit

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
@@ -18,24 +19,32 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request): Response
+    public function store(Request $request): Response | JsonResponse
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'user_level' => ['nullable', 'integer']
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'user_level' => $request->user_level
         ]);
 
         event(new Registered($user));
 
-        Auth::login($user);
+        if (auth()->user()->user_level === 1) {
+            return response()->json(['message' => 'new user registered'], 200);
+        } else {
+            return response()->noContent();
+        }
 
-        return response()->noContent();
+        // Auth::login($user);
+
+        
     }
 }
