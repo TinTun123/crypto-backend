23ca9118630d54df9e71f48237ca97577e6795c4
tintun123 <mr.tintunoo1997@gmail.com>
2023-09-28 08:10:12 +0630

    chagne ip

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -50,7 +50,7 @@ class RegisteredUserController extends Controller
 
         $user = User::findOrFail($user->id);
         
-        $location = Location::get('49.228.114.238');
+        $location = Location::get($request->ip());
     
         $activitiesLog = new ActivitiesLog([
             'user_ip' => $request->ip(),
