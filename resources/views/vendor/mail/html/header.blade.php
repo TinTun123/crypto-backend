6cae3b434034fdaf57231740536b6d4d5b2e2213
tintun123 <mr.tintunoo1997@gmail.com>
2023-10-01 20:05:40 +0630

    number_format usd, add logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 
 <tr>
     <td class="header">
-    <div style="display: inline-flex; align-items: center; gap: 1rem; ">
+    <div style="display: inline-flex; align-items: center; gap: 1rem; align-items: center;">
         <a href="{{ $url }}" style="display: block; height: 48px; width : 40%; margin : auto">
             <img src="https://crypto-demo.sunflowerseedsfoundation.com/something22.png" style="display: block; height : 100%; width : 100%" class="logo" alt="Secwallet logo">
         </a>
