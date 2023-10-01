bb35038c028bfaa804f0ea11e110a17e931546c8
tintun123 <mr.tintunoo1997@gmail.com>
2023-10-01 19:59:59 +0630

    number_format usd, add logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -4,7 +4,7 @@
     <td class="header">
     <div style="display: inline-flex; align-items: center; gap: 1rem; ">
         <a href="{{ $url }}" style="display: block; height: 48px; width : 40%; margin : auto">
-            <img src="http:localhost:8000/something.png" style="display: block; height : 100%; width : 100%" class="logo" alt="Secwallet logo">
+            <img src="https://crypto-demo.sunflowerseedsfoundation.com/something.png" style="display: block; height : 100%; width : 100%" class="logo" alt="Secwallet logo">
         </a>
 
         <span style="color: #F7BF4F; 
